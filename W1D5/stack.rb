d190e4a6fb6582847b595cf7e47e7242b1fa8c6f
Viviene De Guzman <vivienedeg@gmail.com>
2019-01-10 23:12:37 -0800

    Create Stack class

---
+++ W1D5/stack.rb
@@ -0,0 +1,18 @@
+class Stack
+  attr_accessor :stack
+  def initialize
+    @stack = []
+  end
+
+  def push(el)
+    self.stack << el
+  end
+
+  def pop
+    self.stack.pop
+  end
+
+  def peek
+    self.stack.last
+  end
+end

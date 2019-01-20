9ea43f549ac33fc02a05d04454d291104d6d8c99
Viviene De Guzman <vivienedeg@gmail.com>
2019-01-20 11:12:44 -0800

    Delete private resize method and complete LRU cache

--- W2D5/lru_cache.rb
+++ W2D5/lru_cache.rb
@@ -1,35 +1,29 @@
 class LRUCache
   def initialize(size)
     @cache = Array.new(size)
-    @size = size
   end
 
   def count
-    cache.count
+    num = 0
+    cache.each { |el| num += 1 unless el.nil? }
+    num
   end
 
   def add(el)
-    resize(size*2) if count == size
-    # find if el in cache already
+    cache.include?(el) ?  cache.delete(el) : cache.shift
     cache << el
+    cache.dup
   end
 
   def show
-    p cache
+    p cache.dup
   end
 
   private
-  attr_reader :cache, :size
+  attr_reader :cache
 
-  def resize(new_size)
-    size = new_size
-    new_cache = Array.new(size)
-    until cache.empty?
-      new_cache << cache.shift
-    end
-    cache = new_cache
-  end
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   johnny_cache = LRUCache.new(4)
@@ -49,6 +43,5 @@ if __FILE__ == $PROGRAM_NAME
   johnny_cache.add("I walk the line")
   johnny_cache.add({a: 1, b: 2, c: 3})
 
-
   johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
-  end
+end

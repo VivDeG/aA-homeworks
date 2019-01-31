5a1808dc7850b8da6e474662380c4bac69f392df
Viviene De Guzman <vivienedeg@gmail.com>
2019-01-30 19:40:02 -0800

    Complete Rails Auth

--- W4D3/app_name/app/models/user.rb
+++ W4D3/app_name/app/models/user.rb
@@ -1,2 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  attr_reader :password
+
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(username: user_name)
+    return nil if user.nil?
+    if user.is_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+    
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  # def is_password?(password)
+  #   BCrypt::Password.new(self.password_digest).is_password?(password)
+  # end
 end

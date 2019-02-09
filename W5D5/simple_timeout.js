3502524fbc5e5112a2b345f59dd51a86f22813af
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-09 15:17:38 -0800

    Add callback and functions hw

---
+++ W5D5/simple_timeout.js
@@ -0,0 +1,11 @@
+window.setTimeout(() => {
+  alert('HAMMERTIME');
+}, 5000);
+
+function hammerTime(time) {
+  window.setTimeout(() => {
+    alert(`${time} is hammertime!`);
+  });
+}
+
+hammerTime('12:40');
